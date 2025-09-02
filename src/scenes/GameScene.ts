9585fd444b882eb830643dddfd7400df9b3aa9ae
Qwang <qiangsemail@gmail.com>
2025-09-02 00:08:34 -0600

    readability

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import { getTileCenter } from "../util/tile";
 
 export class GameScene extends Phaser.Scene {
   constructor() {
@@ -9,19 +10,26 @@ export class GameScene extends Phaser.Scene {
     if (map) {
       this.setupCamera(map);
       // Add archer sprite at tile (2,2)
+      // Sprite positions
+      const ARCHER_TILE_POS = { x: 2, y: 2 };
+      const CLOUD_TILE_POS = { x: 5, y: 5 };
+
+      const archerPos = getTileCenter(ARCHER_TILE_POS.x, ARCHER_TILE_POS.y);
+      const cloudPos = getTileCenter(CLOUD_TILE_POS.x, CLOUD_TILE_POS.y);
+
       const archer = this.add.sprite(
-        2 * 16 + 8,
-        2 * 16 + 8,
+        archerPos.x,
+        archerPos.y,
         "archer",
-        "idle-1"
+        "idle-0"
       );
       archer.setOrigin(0.5, 0.5);
-      // Add ff7-cloud sprite at tile (5,5)
+
       const cloud = this.add.sprite(
-        5 * 16 + 8,
-        5 * 16 + 8,
+        cloudPos.x,
+        cloudPos.y,
         "ff7-cloud",
-        "idle-1"
+        "idle-0"
       );
       cloud.setOrigin(0.5, 0.5);
     }
