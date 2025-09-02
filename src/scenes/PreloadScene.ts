8637f7d732c469e2c0f029490778bd848d446e83
Qwang <qiangsemail@gmail.com>
2025-09-01 22:59:18 -0600

    Add arcade font assets including PNG and XML files
    - Added arcade.png to the fonts directory, containing the pixel data for the arcade font.
    - Created arcade.xml to define the font properties, character mappings, and layout for the arcade font.

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -5,9 +5,37 @@ export class PreloadScene extends Phaser.Scene {
     super("Preload");
   }
   preload() {
-    // Preload assets for Menu/Game
+    // Bitmap font (arcade)
+    this.load.bitmapFont(
+      "arcade",
+      "assets/fonts/arcade.png",
+      "assets/fonts/arcade.xml"
+    );
+    // TTF webfont (VT323)
+    this.load.script(
+      "webfont",
+      "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+    );
   }
   create() {
-    this.scene.start("Menu");
+    // Load VT323 webfont using WebFont Loader
+    // @ts-ignore
+    if ((window as any).WebFont) {
+      // @ts-ignore
+      (window as any).WebFont.load({
+        custom: {
+          families: ["VT323"],
+          urls: ["assets/fonts/VT323-Regular.ttf"],
+        },
+        active: () => {
+          this.scene.start("Menu");
+        },
+        inactive: () => {
+          this.scene.start("Menu");
+        },
+      });
+    } else {
+      this.scene.start("Menu");
+    }
   }
 }
